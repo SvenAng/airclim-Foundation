3ee50ed146cd6ed0ccf3931536e2096dd67a0da1
svenA <svenangermark@Svens-MacBook-Pro-2.local>
2017-02-15 20:44:07 +0100

    15feb

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function airclim_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function airclim_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function airclim_preprocess_node(&$variables) {
+}
